77addf499e8f4279194bc68c85bad58c9566ebb8
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2022-01-13 12:33:30 -0600

    peerfs is cool

--- node.py
+++ node.py
@@ -1,16 +1,29 @@
 from flask import Flask
 import platform
 import psutil
+import socket
 app = Flask(__name__)
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 @app.route('/')
 def index():
-    return "You've located a peerFS node! Good job!"
+    return "peerFS node running on public ip %s" % get_ip()
 
 @app.route('/fetchstats')
 def fetchstats():
     # returns the host OS, CPU, and memory usage
-    return "Host OS: " + platform.system() + "\n" + "CPU: " + str(psutil.cpu_percent()) + "%\n" + "Memory: " + str(psutil.virtual_memory().percent) + "%\n" + "Public IP: " + str(psutil.net_if_addrs()['eth0'][0][1])
+    return "Host OS: " + platform.system() + "\n" + "CPU: " + str(psutil.cpu_percent()) + "%\n" + "Memory: " + str(psutil.virtual_memory().percent) + "%\n" + "Public IP: " + get_ip()
 
 if __name__ == '__main__':
     app.run(host="192.168.1.115", port=18623)
