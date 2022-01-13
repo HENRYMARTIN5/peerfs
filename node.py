2fbefd462099d75ed7e07e8a5e5b78442ad7a63d
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2022-01-13 11:54:47 -0600

    commit

---
+++ node.py
@@ -0,0 +1,14 @@
+# A simple flask app that responds to requests with the current node's IP address
+from flask import Flask
+import platform
+import psutil
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "You've located a peerFS node! Good job!"
+
+@app.route('/fetchstats')
+def fetchstats():
+    # returns the host OS, CPU, and memory usage
+    return "Host OS: " + platform.system() + "\n" + "CPU: " + str(psutil.cpu_percent()) + "\n" + "Memory: " + str(psutil.virtual_memory().percent)
